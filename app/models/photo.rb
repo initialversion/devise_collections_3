059f71c8377abc61f8932d4a881bcf68e5eec101
Raghu Betina <raghu@firstdraft.com>
2016-10-19 23:36:09 +0000

    Direct association between photographers and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :owner,
+             :class_name => "Photographer"
+
   # Indirect associations
 
   # Validations
--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,6 +1,10 @@
 class Photographer < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :foreign_key => "owner_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
